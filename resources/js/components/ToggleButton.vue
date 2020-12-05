51994e801a5b20ac4a69cfb4729d91e86ba1b962
Jamie Jansen <jamie@jnsn.me>
2020-12-05 15:51:03 -0800

    Create toggle button component

---
+++ resources/js/components/ToggleButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="ui-toggle" :class="on ? 'ui-toggle-on' : 'ui-toggle-off'">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'user-id',
+            'type',
+            'on'
+        ]
+    }
+</script>
+
+<style scoped>
+    .ui-toggle {
+        cursor: pointer;
+    }
+</style>
