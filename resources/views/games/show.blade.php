efec4c1e88eda5611d18f15f5d9f338dccde1479
Jamie Jansen <jamie@jnsn.me>
2021-02-21 08:32:33 -0800

    Tidy up game page

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -1,43 +1,47 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="game-background-container">
-        <div class="game-background" style="background-image:url({{ $game->background_path }})"></div>
+    <div class="feature-image-container">
+        <div class="feature-image" style="background-image:url({{ $game->background_path }})"></div>
+        <div class="feature-image-overlay"></div>
     </div>
 
-    <div class="container game-info-container">
-        <header class="game-header columns">
-            <div class="column is-one-quarter">
+    <div class="container game-page">
+        <div class="columns">
+            <div class="column is-one-fifth">
                 <img src="{{ $game->cover_path }}" class="game-cover" alt="Cover image for {{ $game->name }}" />
 
-                @foreach($game->files as $file)
-                    @if($file->path_exists)
-                        <a href="{{ $file->download_url }}" class="button is-primary is-fullwidth is-medium download-button">
-                            <icon type="download"></icon>
-                            <span>Download  ({{ $file->platform->metadata->abbreviation }})</span>
-                        </a>
-                    @endif
-                @endforeach
+                <div class="buttons">
+                    @foreach($game->files as $file)
+                        @if($file->path_exists)
+                            <a href="{{ $file->download_url }}" class="button is-primary is-fullwidth is-medium download-button">
+                                <icon type="download"></icon>
+                                <span>Download  ({{ $file->platform->metadata->abbreviation }})</span>
+                            </a>
+                        @endif
+                    @endforeach
+                </div>
             </div>
-    
-            <div class="column game-info">
-                <div class="columns">
-                    <div class="column game-info-main">
-                        <h1 class="game-name">{{ $game->name }}</h1>
 
+            <div class="column">
+                <div class="columns">
+                    <div class="column px-6">
+                        <h1 class="game-name">{{ $game->name }}</h1>
                         <div class="content">
                             <p>{!! $game->description !!}</p>
                         </div>
                     </div>
 
-                    <div class="column game-info-sidebar is-one-quarter">
-                        <a href="https://www.igdb.com/games/{{ $game->slug }}" class="button is-primary is-large is-fullwidth" target="new">IGDB</a>
-                        @foreach($game->link_list as $link)
-                            <a href="{{ $link["url"] }}" class="button is-white is-fullwidth" target="new">{{ $link["category"] }}</a>
-                        @endforeach
+                    <div class="column is-one-quarter">
+                        <div class="buttons">
+                            <a href="https://www.igdb.com/games/{{ $game->slug }}" class="button is-primary is-large is-fullwidth" target="new">IGDB</a>
+                            @foreach($game->link_list as $link)
+                                <a href="{{ $link["url"] }}" class="button is-white is-fullwidth" target="new">{{ $link["category"] }}</a>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
     </div>
 @endsection
